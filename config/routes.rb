2c90e7f32c1551fc42f2284a3ab62f8dff6c3bfd
ui  mizuhara <ui.mizuhara@gmail.com>
2017-12-31 06:52:47 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  root to: "tasks#index"
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  root to: "toppages#index"
+  
+  get "login", to:"sessions#new"
+  post "login", to:"sessions#create"
+  delete "logout", to:"sessions#destroy"
+  
   resources :tasks
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:new, :create, :show]
 end
