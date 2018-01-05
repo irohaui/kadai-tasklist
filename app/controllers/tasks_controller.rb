67a432724e3198376087064166cd683ee1c134b3
ui  mizuhara <ui.mizuhara@gmail.com>
2018-01-05 11:32:20 +0000

    modify bug

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user
+  before_action :correct_user, only: [:index, :show, :new, :edit, :update, :destroy]
+  
   
   def index
     @tasks = Task.all
